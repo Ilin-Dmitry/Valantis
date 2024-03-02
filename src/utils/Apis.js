4843929493e09457147cd5f0a31c149eec3a7e30
Ilin-Dmitry <dm-ilin@mail.ru>
2024-03-02 14:55:25 +0400

    No creds

--- src/utils/Apis.js
+++ src/utils/Apis.js
@@ -5,7 +5,6 @@ export async function fetchIds() {
     const res = await fetch(API_LINK, {
       method: "POST",
       headers: HEADERS,
-      credentials: "include",
       body: JSON.stringify({ action: "get_ids" }),
     });
     if (!res.ok)
@@ -23,7 +22,6 @@ export async function fetchItems(itemsArray) {
     const res = await fetch(API_LINK, {
       method: "POST",
       headers: HEADERS,
-      credentials: "include",
       body: JSON.stringify({
         action: "get_items",
         params: { ids: itemsArray },
@@ -44,7 +42,6 @@ export async function fetchFilteredIds(type, value) {
     const res = await fetch(API_LINK, {
       method: "POST",
       headers: HEADERS,
-      credentials: "include",
       body: JSON.stringify({
         action: "filter",
         params: { [type]: value },
